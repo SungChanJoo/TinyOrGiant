4c48e4e6d8285c12efe76338e4399351c64b5ca3
msjung8634 <msjung8634@gmail.com>
2023-12-26 16:26:08 +0900

    [Create] Implements Physics / Non-Physics Hand
    - Non-Physics Hand 추가
       Physics Hand와 거리가 멀어지는 경우 붉은 색으로 표시.
    
    - Physics Hand Collider에 Physics Material 추가
       Friction / Bounciness는 일단 0으로 지정.
    * Physics Hand의 Rigidbody mass는 100으로 지정
      이후에 100을 기준으로 상대적 무게 설정 필요함.
    
    [done]

--- Assets/1.Scene/MSJ/3.Script/HandPresencePhysics.cs
+++ Assets/1.Scene/MSJ/3.Script/HandPresencePhysics.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class HandPresencePhysics : MonoBehaviour
 {
+    [Header("Non-Physical Hand")]
     public Transform target;
+    public bool showNonPhysicalHand = true;
+    public Renderer nonPhysicalHand;
+    public float distanceThreshold = .3f;
+
+    [Header("Physical Hand")]
+    public float colliderEnableDelay = .5f;
+    Collider[] handColliders;
     Rigidbody rb;
 
     private void Awake()
     {
         TryGetComponent(out rb);
+        handColliders = GetComponentsInChildren<Collider>();
     }
 
     private void FixedUpdate()
@@ -24,5 +33,39 @@ public class HandPresencePhysics : MonoBehaviour
         Vector3 rotationDiffInDegree = angleInDegree * rotationAxis;
 
         rb.angularVelocity = (rotationDiffInDegree * Mathf.Deg2Rad) / Time.fixedDeltaTime;
+    }
+
+    private void Update()
+    {
+        if (showNonPhysicalHand)
+        {
+            float distance = Vector3.Distance(transform.position, target.position);
+            nonPhysicalHand.enabled = distance < distanceThreshold ? false : true;
+        }
+    }
+
+    // XR Direct Interactor Event
+    public void ToggleCollider(bool isEnable)
+    {
+        if (isEnable)
+            Invoke("EnableHandCollider", colliderEnableDelay);
+        else
+            DisableHandCollider();
+    }
+
+    public void EnableHandCollider()
+    {
+        foreach (var handCollider in handColliders)
+        {
+            handCollider.enabled = true;
+        }
+    }
+
+    public void DisableHandCollider()
+    {
+        foreach (var handCollider in handColliders)
+        {
+            handCollider.enabled = false;
+        }
     }
 }
