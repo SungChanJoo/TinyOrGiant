90213aef16857b8b436c204e6b55151cf8509cbc
SungChanJoo <scjoo123@naver.com>
2024-01-11 10:05:01 +0900

    [feat] DealDamage 동기화 추가

--- Assets/1.Scene/JSC/3.Script/Player/DealDamage.cs
+++ Assets/1.Scene/JSC/3.Script/Player/DealDamage.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class DealDamage : MonoBehaviour
+public class DealDamage : NetworkBehaviour
 {
     public float defaultDamage = 1f;
     public LayerMask targetLayer;
@@ -13,7 +14,17 @@ public class DealDamage : MonoBehaviour
 
         var targetHealth = collision.gameObject.GetComponent<Health>();
         if (targetHealth == null) return;
+        CmdTakeDamage(targetHealth , defaultDamage);
+    }
+    [Command]
+    public void CmdTakeDamage(Health targetHealth, float damage)
+    {
+        RpcTakeDamage(targetHealth, damage);
+    }
+    [ClientRpc]
+    public void RpcTakeDamage(Health targetHealth, float damage)
+    {
+        targetHealth.TakeDamage(damage);
 
-        targetHealth.TakeDamage(defaultDamage);
     }
 }
