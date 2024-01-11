7e2b8782fa6f33d67d3bdacfcefdbe7b3e517f4c
SungChanJoo <scjoo123@naver.com>
2024-01-11 15:47:54 +0900

    [fix] 플레이어 스폰위치 +메인카메라 위치변경

--- Assets/1.Scene/JSC/3.Script/ChoiceNetworkManager.cs
+++ Assets/1.Scene/JSC/3.Script/ChoiceNetworkManager.cs
@@ -6,41 +6,17 @@ using Mirror;
 
 public class ChoiceNetworkManager : NetworkManager
 {
-    public Vector3 PlayerPos = new Vector3(0, 5f, 0);
+    public Transform[] PlayerSpawnPos;
+    private int _PlayerCount=0;
     public void ReplacePlayer(NetworkConnectionToClient conn, GameObject newPrefab)
     {
         GameObject oldPlayer = conn.identity.gameObject;
+        NetworkServer.ReplacePlayerForConnection(conn, Instantiate(newPrefab, PlayerSpawnPos[_PlayerCount].position, Quaternion.identity), true);
+        _PlayerCount++;
+        if (_PlayerCount >= PlayerSpawnPos.Length)
+            _PlayerCount = 0;
 
-        //GameObject XRMainCam = null;
-        //var cameras = FindObjectsOfType<Camera>(true);
-        //foreach (var camera in cameras)
-        //{
-        //    if (camera.gameObject.name.Equals("XR Main Camera"))
-        //        XRMainCam = camera.gameObject;
-        //}
-        //if (XRMainCam != null) XRMainCam.transform.SetParent(null);
-
-        NetworkServer.ReplacePlayerForConnection(conn, Instantiate(newPrefab, PlayerPos, Quaternion.identity), true);
         Destroy(oldPlayer, 0.1f);
     }
-
-    //public GameObject leftHandPresencePhysicsPrefab;
-    //public GameObject rightHandPresencePhysicsPrefab;
-
-    //public void CreateVRPhysicalHands()
-    //{
-    //    StartCoroutine(DelayedCreateVRPhysicalHands());
-    //}
-
-    //private IEnumerator DelayedCreateVRPhysicalHands()
-    //{
-    //    yield return new WaitForSeconds(1f);
-
-    //    var leftHandPresenceObject = Instantiate(leftHandPresencePhysicsPrefab);
-    //    var rightHandPresenceObject = Instantiate(rightHandPresencePhysicsPrefab);
-
-    //    NetworkServer.Spawn(leftHandPresenceObject);
-    //    NetworkServer.Spawn(rightHandPresenceObject);
-    //}
 }
 
